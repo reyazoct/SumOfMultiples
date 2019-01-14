19ff9f52c288d93c61eaeeb054582a62ed009e2b
Reyaz Ahmad <reyazoct@gmail.com>
2019-01-14 23:44:50 +0530

    add test for range (3,5) with limit 4

--- lib/sumofmultiples.rb
+++ lib/sumofmultiples.rb
@@ -1,9 +1,18 @@
 class SumOfMultiples
-  def initialize(first,second)
+  def initialize(first, second)
     @first = first
     @second = second
   end
+
   def to(range)
-    0
+    sum = 0
+    [@first, @second].each do |num|
+      x = num
+      while x < range
+        sum += x
+        x += num
+      end
+    end
+    sum
   end
 end
--- spec/sumofmultiples_spec.rb
+++ spec/sumofmultiples_spec.rb
@@ -2,7 +2,12 @@ require 'sumofmultiples'
 
 describe SumOfMultiples do
   it "test for range (3,5) with limit 1" do
-    som = SumOfMultiples.new(3,5)
+    som = SumOfMultiples.new(3, 5)
     expect(som.to(1)).to eq 0
+  end
+
+  it "test for range (3,5) with limit 4" do
+    som = SumOfMultiples.new(3, 5)
+    expect(som.to(4)).to eq 3
   end
 end
